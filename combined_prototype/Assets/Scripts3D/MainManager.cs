43826f1f30b5e368a9aeb6243af8c0d431fb7f67
daspiegs <doriaspiegel@mde.harvard.edu>
2021-05-05 00:14:17 -0400

    WAY BETTER UI on 3d

--- combined_prototype/Assets/Scripts3D/MainManager.cs
+++ combined_prototype/Assets/Scripts3D/MainManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,15 @@ using UnityEngine;
 public class MainManager : MonoBehaviour
 {
 
+    //stuff for UI showing up 
+    public static int fiveHunMeasured = 0;
+    public static int ninetyMeasured = 0;
+    public static int thirtyMeasured = 0;
+    public GameObject check90;
+    public GameObject check30;
+    public GameObject check500;
+
+    //
     private bool angle3done = false;
     private bool angle2done = false;
     private bool dist3done = false;
@@ -15,7 +25,9 @@ public class MainManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        check90.SetActive(false);
+        check30.SetActive(false);
+        check500.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,6 +37,24 @@ public class MainManager : MonoBehaviour
         {
             AllDone.SetActive(true);
             AllDone2.SetActive(true);
+        }
+
+        //show dist check 
+        if (dist3done || dist2done)
+        {
+            check500.SetActive(true);
+        }
+
+        //show 90 check
+        if (angle3done)
+        {
+            check90.SetActive(true);
+        }
+
+        //show 30 check 
+        if (angle2done)
+        {
+            check30.SetActive(true);
         }
     }
 
